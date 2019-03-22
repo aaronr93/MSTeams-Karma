06f13cbc782c5875dd7bfa8ff49443fbd1ab1d4d
Aaron Rosenberger <arosenberger@silverchair.com>
2019-03-22 07:53:43 -0400

    Continue trying to get it to work locally

--- MSTeams-Karma/Util/KeyVaultHelper.cs
+++ MSTeams-Karma/Util/KeyVaultHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Configuration;
 using Microsoft.Azure.KeyVault;
@@ -9,13 +10,15 @@ namespace MSTeams.Karma.Util
     public class KeyVaultHelper
     {
         private static string KeyVaultBaseUri => WebConfigurationManager.AppSettings["AzureKeyVaultBaseUri"];
+        private static readonly HttpClient HttpClient = new HttpClient();
 
         public static string GetSecret(string key)
         {
             AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
 
             var keyVaultClient = new KeyVaultClient(
-                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                    new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback),
+                    HttpClient);
 
             return keyVaultClient.GetSecretAsync($"{KeyVaultBaseUri}/secrets/{key}").Result.Value;
         }
